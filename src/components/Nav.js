f5a89f9f530d6eec8e32b53f3dc3eb97e2db03be
san <sansamaa0@gmail.com>
2023-05-13 21:13:14 +0300

    h

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,16 +2,24 @@ import React from "react";
 import piggy from "../assets/porco.png";
 
 const Nav = () => {
-	return (
-		<div className="navWrapper">
-			<span className="headerText">HogWild</span>
-			<div className="TwirlyPig">
-				<img src={piggy} className="App-logo" alt="piggy" />
-			</div>
-			<span className="normalText">
-				A React App for County Fair Hog Fans
-			</span>
-		</div>
+	return ( <
+		div className = "navWrapper" >
+		<
+		span className = "headerText" > HogWild < /span> <
+		div className = "TwirlyPig" >
+		<
+		img src = {
+			piggy
+		}
+		className = "App-logo"
+		alt = "piggy" / >
+		<
+		/div> <
+		span className = "normalText" >
+		A React App
+		for County Fair Hog Fans <
+		/span> <
+		/div>
 	);
 };
 
